9db6d3692bee078a603580b5fb9f9012c7a05972
Laurence Stokes <laurence_stokes@hotmail.com>
2015-03-03 00:24:56 +0000

    added oauth callback for Heroku

--- config/auth.js
+++ config/auth.js
@@ -16,3 +16,27 @@ module.exports = {
     }
 
 };
+
+
+//HEROKU
+
+/**
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+        'clientID'      : '1592265807686674', 
+        'clientSecret'  : 'cb4b2a912ae1668257fcd6d81decc6e7', 
+        'callbackURL'   : 'https://codefighter.herokuapp.com/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'VRLpSApKpKBkbOg9HKUkIUUKe',
+        'consumerSecret'    : 'U63VnZ3PU7TyK2MzNqZQIKAtMKiwgGom9kat1pXaoMXLHGRZoY',
+        'callbackURL'       : 'https://codefighter.herokuapp.com/auth/twitter/callback'
+    }
+
+};
+*//
