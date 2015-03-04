c1d9d47feacce0fcd0ada86ecc3104da2844e066
Laurence Stokes <laurence_stokes@hotmail.com>
2015-03-04 15:39:02 +0000

    removed unncessary images

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@
 module.exports = {
 	
 	//localhost
-	'url' : 'mongodb://localhost/db'
+	//'url' : 'mongodb://localhost/db'
 	
 	//heroku
-	//'url': 'mongodb://heroku_app34521617:vjg0qohk4bj0dospefd33kd712@ds049211.mongolab.com:49211/heroku_app34521617'
+	'url': 'mongodb://heroku_app34521617:vjg0qohk4bj0dospefd33kd712@ds049211.mongolab.com:49211/heroku_app34521617'
 
 };
